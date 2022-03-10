c6d3f95f4319007e4d97fe22b516a501def4d76c
TeaStudios <50765357+TeaStudios@users.noreply.github.com>
2022-03-10 11:38:55 +1100

    blank project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cry_locker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
